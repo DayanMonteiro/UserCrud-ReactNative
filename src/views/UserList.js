c39383780796f67e927278be3809d23649f822d1
DayanMonteiro <dmsmamb@gmail.com>
2022-10-24 18:51:29 -0300

    user list

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,35 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import { Avatar, ListItem } from '@rneui/base'
+import users from '../data/users'
 
 export default props => {
+
+    function getUserItem({ item: user }) {
+            return (
+                <ListItem
+                    bottomDivider
+                    onPress={() => props.navigation.navigate('UserForm')}
+                >
+                <Avatar  title={user.name} source={{ uri: user.avatarUrl }}/>
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron />
+            </ListItem>
+            ) 
+
+
+    }
+
     return (
-        <Text>User List</Text>
+        <View>
+            <FlatList 
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
