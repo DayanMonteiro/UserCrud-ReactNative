4a00b9531f01dcb20e6b29bff60363fe2e247072
DayanMonteiro <dmsmamb@gmail.com>
2022-10-25 18:31:01 -0300

    user list

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,35 +1,71 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, SafeAreaView, Alert } from 'react-native'
 import { Avatar, ListItem } from '@rneui/base'
 import users from '../data/users'
+import { Button } from '@rneui/themed'
+import { Icon } from 'react-native-elements'
 
 export default props => {
-
+ 
+    function confirmUserDelete(user) {
+        Alert.alert("Excluir usuário", "Deseja excluir o usuário?", [
+            {
+                text: "Sim ",
+                onPress() {
+                    console.warn("Delete" + user.id)
+                }
+            }, { text: "Não" }
+        ])
+    }
+ 
+    function getActions(user) {
+        return (
+            <>
+                <Button
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                    type="clear"
+                />
+                <Button
+                    onPress={() => confirmUserDelete(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+ 
     function getUserItem({ item: user }) {
-            return (
-                <ListItem
-                    bottomDivider
-                    onPress={() => props.navigation.navigate('UserForm')}
-                >
-                <Avatar  title={user.name} source={{ uri: user.avatarUrl }}/>
+        return (
+            <ListItem
+                onPress={() => props.navigation.navigate('UserForm', user)}
+                bottomDivider
+            >
+                <Avatar title={user.name} source={{ uri: user.avatarUrl }} />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
                 </ListItem.Content>
-                <ListItem.Chevron />
+                <ListItem.Content 
+                    style={{ 
+                        flexDirection: 'row', 
+                        justifyContent: 'flex-end', 
+                        flexGrow: 0.3 
+                    }}
+                >
+                    {getActions(user)}
+                </ListItem.Content>
             </ListItem>
-            ) 
-
-
+        )
     }
-
+ 
     return (
-        <View>
-            <FlatList 
-                keyExtractor={user => user.id.toString()}
+        <SafeAreaView>
+            <FlatList
                 data={users}
+                keyExtractor={user => user.id.toString()}
                 renderItem={getUserItem}
             />
-        </View>
+        </SafeAreaView>
     )
 }
